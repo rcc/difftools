042c63c1b544699c4ff159a6cdb7e0d98a647aa3
Robert Curtis <robert.caston@gmail.com>
2010-04-28 15:02:14 -0700

    support command running by detecting name of argv[0]

--- src/difftools.c
+++ src/difftools.c
@@ -20,20 +20,48 @@
 #include <cmds.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
 
 int main(int argc, const char * argv[])
 {
 	int ret;
 	int status = 0;
+	char *cmdname;
+	char *arg0;
 
-	pdebug("Version: %s\n", SCM_HASH);
-
-	if((ret = run_cmds(argc-1, &argv[1], NULL)) < 0) {
-		fprintf(stderr,
-			"An error occurred with command at position %d\n",
-			-ret);
+	/* create the command string */
+	if((arg0 = malloc(strlen(argv[0]))) == NULL) {
+		perror("Could not allocate arg0");
 		status = -1;
 		goto exit;
+	}
+	strcpy(arg0, argv[0]);
+	if((cmdname = basename(arg0)) == NULL) {
+		perror("Could not get basename of command");
+		status = -1;
+		goto exit;
+	}
+
+	pdebug("Command: %s\n", cmdname);
+	pdebug("Version: %s\n", SCM_HASH);
+
+	if(strcmp("difftools", cmdname) == 0) {
+		if((ret = run_cmds(argc-1, &argv[1], NULL)) < 0) {
+			fprintf(stderr,
+				"An error occurred with command at position "
+				"%d\n", -ret);
+			status = -1;
+			goto exit;
+		}
+	} else {
+		/* treat the argv[0] command name as a command */
+		status = run_cmd(cmdname, argc - 1, &argv[1], NULL);
+		if(status != 0)
+			fprintf(stderr,
+				"An error occurred with command: %s\n",
+				cmdname);
 	}
 
 exit:
